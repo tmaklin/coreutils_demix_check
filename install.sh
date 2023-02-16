2a0ee9a77b28163b8cb8cee8beb447c84471be7e
Tommi Mäklin <tommi@maklin.fi>
2023-02-16 19:14:00 +0000

    Add install script for dependencies

---
+++ install.sh
@@ -0,0 +1,15 @@
+wget http://ftp.gnu.org/gnu/datamash/datamash-1.8.tar.gz
+tar -xzf datamash-1.8.tar.gz
+cd datamash-1.8
+./configure
+make
+
+cd ../
+wget -O seqtk-v1.3.tar.gz https://github.com/lh3/seqtk/archive/refs/tags/v1.3.tar.gz
+tar -xzf seqtk-v1.3.tar.gz
+cd seqtk-1.3
+make
+
+cd ../
+tar -xf mash-Linux64-v2.3.tar
+
