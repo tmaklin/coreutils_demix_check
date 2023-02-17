85fc3530b9b736b54e4dde831414814c4d0324cc
Tommi Mäklin <tommi@maklin.fi>
2023-02-17 15:10:40 +0000

    Implement creating ref_msh_dis_clu.tsv from ref_msh_dis.tsv using coreutils and awk (remove slow loops calling grep in each iteration).

--- setup_reference.sh
+++ setup_reference.sh
@@ -4,10 +4,9 @@
 
 ##set -euxo pipefail
 
-paths=tmp_paths_$RANDOM".txt"
+paths=ref_paths.txt
 cut -f3 $1 | sed '1d' > $paths
 mash sketch -p $2 -s 10000 -o ref -l $paths 2> /dev/null
-rm $paths
 mash dist -p $2 ref.msh ref.msh | pigz -11 -p $2 > ref_msh_dis.tsv.gz
 
 touch ref_comp.tsv
@@ -28,68 +27,22 @@ touch ref_clu_comp.tsv
 echo -e "cluster\tn\tlength_ave\tlength_min\tlength_max" > ref_clu_comp.tsv
 sed '1d' ref_comp.tsv | datamash --sort --group 2 count 2 mean 3 min 3 max 3 >> ref_clu_comp.tsv
 
-ref_path=tmp_1_$RANDOM".txt"
-met_path=tmp_2_$RANDOM".txt"
-distance=tmp_3_$RANDOM".txt"
-prob=tmp_4_$RANDOM".txt"
-hashes=tmp_5_$RANDOM".txt"
-ss=tmp_6_$RANDOM".txt"
-
-zcat --force ref_msh_dis.tsv.gz | cut -f1 > $ref_path
-zcat --force ref_msh_dis.tsv.gz | cut -f2 > $met_path
-zcat --force ref_msh_dis.tsv.gz | cut -f3 > $distance
-zcat --force ref_msh_dis.tsv.gz | cut -f4 > $prob
-zcat --force ref_msh_dis.tsv.gz | cut -f5 | cut -f1 -d'/' > $hashes
-zcat --force ref_msh_dis.tsv.gz | cut -f5 | cut -f2 -d'/' > $ss
-
-ref_id=tmp_7_$RANDOM".txt"
-met_id=tmp_8_$RANDOM".txt"
-
-ref_cluster=tmp_9_$RANDOM".txt"
-
-while read line; do
-    info=$(grep $line $1 | head -n1)
-    id=$(echo $info | cut -f1 -d' ')
-    cluster=$(echo $info | cut -f2 -d' ')
-    echo $id >> $ref_id
-    echo $cluster >> $ref_cluster
-done < $ref_path
-
-met_cluster=tmp_10_$RANDOM".txt"
-
-while read line; do
-    info=$(grep $line $1 | head -n1)
-    id=$(echo $info | cut -f1 -d' ')
-    cluster=$(echo $info | cut -f2 -d' ')
-    echo $id >> $met_id
-    echo $cluster >> $met_cluster
-done < $met_path
-
-category=tmp_11_$RANDOM".txt"
-
-while IFS= read -r ref_cl && IFS= read -r met_cl <&3; do
-    if [[ "$ref_cl" == "$met_cl" ]]; then
-	echo "same cluster"
-    else
-	echo "different cluster"
-    fi
-done < $ref_cluster 3< $met_cluster > $category
+## Sort the ref info for `join`
+sort ref_info.tsv > ref_info.sorted.tsv
 
 echo -e "ref_id\tmet_id\tdistance\thashes\tss\tp\tref_cluster\tmet_cluster\tcategory" > ref_msh_dis_clu.tsv
-paste $ref_id $met_id $distance $hashes $ss $prob $ref_cluster $met_cluster $category >> ref_msh_dis_clu.tsv
+zcat --force ref_msh_dis.tsv.gz \
+    | sort -k1 \
+    | join -1 1 -2 3 - ref_info.sorted.tsv \
+    | tr ' ' '\t' | sort -k2 \
+    | join -1 2 -2 3 - ref_info.sorted.tsv \
+    | tr ' ' '\t' \
+    | sed 's/\([0-9][0-9]*\)[/]\([0-9][0-9]*\)/\1\t\2/g' \
+    | awk '{ print $9 "\t" $7 "\t" $3 "\t" $5 "\t" $6 "\t" $4 "\t" $10 "\t" $8}' \
+    | awk -F"[\t]" '{print $0, "\t" ($7==$8?"same":"different") "_cluster"}' \
+    | tr -d ' ' \
+    | sed 's/_cluster/ cluster/g' >> ref_msh_dis_clu.tsv
 
 Rscript get_thresholds.R
 
 pigz --force -11 -p $2 ref_msh_dis_clu.tsv
-
-rm $ref_path
-rm $met_path
-rm $distance
-rm $prob
-rm $hashes
-rm $ss
-rm $ref_id
-rm $met_id
-rm $ref_cluster
-rm $met_cluster
-rm $category
